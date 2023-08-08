266f96fa98dff4c447b128a1ab835e0c8c27614b
marcelo1394 <harold_marcelo@hotmail.com>
2023-08-08 14:57:40 -0500

    creando botones

--- src/app/botones/botones.module.ts
+++ src/app/botones/botones.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './pages/home/home-page.component';
+import { BotonComponent } from './components/boton/boton.component';
 
 
 
 @NgModule({
   declarations: [
-    HomePageComponent
+    HomePageComponent,
+    BotonComponent
   ],
   imports: [
     CommonModule,
